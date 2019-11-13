472c47032766d48721850f912eeb1002cf79b6c9
cmkishores <cmkishores@gmail.com>
2019-11-13 15:46:18 +0530

    Add react-router-dom , add Contact listing and CreateUpdate components

---
+++ frontend/src/ContactCreateUpdate.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import ContactManager from './ContactManager';
+
+const ContactManager = new ContactManager();
+
+class customerCreateUpdate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      componentDidMount(){
+        const { match: { params } } = this.props;
+        if(params && params.pk)
+        {
+          ContactManager.getContact(params.pk).then((c)=>{
+            this.refs.firstName.value = c.first_name;
+            this.refs.lastName.value = c.last_name;
+            this.refs.email.value = c.email;
+            this.refs.phone.value = c.phone;
+            this.refs.description.value = c.description;
+            this.refs.organization.value = c.organization;
+            this.refs.title.value = c.title;
+
+
+
+          })
+        }
+      }
+
+      handleCreate(){
+        ContactManager.createContact(
+          {
+            "first_name": this.refs.firstName.value,
+            "last_name": this.refs.lastName.value,
+            "email": this.refs.email.value,
+            "phone": this.refs.phone.value,
+        }          
+        ).then((result)=>{
+          alert("Contact created!");
+        }).catch(()=>{
+          alert('There was an error! Please re-check your form.');
+        });
+      }
+      handleUpdate(pk){
+        ContactManager.updateContact(
+          {
+            "pk": pk,
+            "first_name": this.refs.firstName.value,
+            "last_name": this.refs.lastName.value,
+            "email": this.refs.email.value,
+            "phone": this.refs.phone.value,
+        }          
+        ).then((result)=>{
+          console.log(result);
+          alert("Contact updated!");
+        }).catch(()=>{
+          alert('There was an error! Please re-check your form.');
+        });
+      }
+      handleSubmit(event) {
+        const { match: { params } } = this.props;
+
+        if(params && params.pk){
+          this.handleUpdate(params.pk);
+        }
+        else
+        {
+          this.handleCreate();
+        }
+
+        event.preventDefault();
+      }
+
+      render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>
+              First Name:</label>
+              <input className="form-control" type="text" ref='firstName' />
+
+            <label>
+              Last Name:</label>
+              <input className="form-control" type="text" ref='lastName'/>
+
+            <label>
+              Phone:</label>
+              <input className="form-control" type="text" ref='phone' />
+
+            <label>
+              Email:</label>
+              <input className="form-control" type="text" ref='email' />
+
+            <label>
+              Address:</label>
+              <input className="form-control" type="text" ref='address' />
+
+            <label>
+              Description:</label>
+              <textarea className="form-control" ref='description' ></textarea>
+
+
+            <input className="btn btn-primary" type="submit" value="Submit" />
+            </div>
+          </form>
+        );
+      }  
+}
+
+export default customerCreateUpdate;
---
+++ frontend/src/ContactList.js
@@ -0,0 +1,90 @@
+import  React, { Component } from  'react';
+import  ContactManager  from  './ContactManager';
+
+const  contactManager  =  new  ContactManager();
+
+class  ContactList  extends  Component {
+
+    constructor(props) {
+        super(props);
+        this.state  = {
+            contacts: [],
+            nextPageURL:  ''
+        };
+        this.nextPage  =  this.nextPage.bind(this);
+        this.handleDelete  =  this.handleDelete.bind(this);
+    }
+    
+    componentDidMount() {
+        var  self  =  this;
+        contactManager.getContacts().then(function (result) {
+            console.log(result);
+            self.setState({ contacts:  result.data, nextPageURL:  result.nextlink})
+        });
+    }
+    handleDelete(e,pk){
+        var  self  =  this;
+        contactManager.deleteContact({pk :  pk}).then(()=>{
+            var  newArr  =  self.state.contacts.filter(function(obj) {
+                return  obj.pk  !==  pk;
+            });
+    
+            self.setState({contacts:  newArr})
+        });
+    }
+    
+    nextPage(){
+        var  self  =  this;
+        console.log(this.state.nextPageURL);        
+        contactManager.getCcontactsByURL(this.state.nextPageURL).then((result) => {
+            self.setState({ contacts:  result.data, nextPageURL:  result.nextlink})
+        });
+    }
+    render() {
+    
+        return (
+            <div  className="contacts--list">
+                <table  className="table">
+                <thead  key="thead">
+                <tr>
+
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Description</th>
+                    <th>Organization</th>
+                    <th>Title</th>
+
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.contacts.map( contact  =>
+                    <tr  key={c.pk}>
+                    <td>{contact.pk}  </td>
+                    <td>{contact.first_name}</td>
+                    <td>{contact.last_name}</td>
+                    <td>{contact.phone}</td>
+                    <td>{contact.email}</td>
+                    <td>{contact.description}</td>
+                    <td>{contact.organization}</td>
+                    <td>{contact.title}</td>
+                    <td>{contact.createdAt}</td>
+
+
+
+                    <td>
+                    <button  onClick={(e)=>  this.handleDelete(e,contact.pk) }> Delete</button>
+                    <a  href={"/contact/" + contact.pk}> Update</a>
+                    </td>
+                </tr>)}
+                </tbody>
+                </table>
+                <button  className="btn btn-primary"  onClick=  {  this.nextPage  }>Next</button>
+            </div>
+            );
+      }
+    }
+    export  default  ContactList;
+    
