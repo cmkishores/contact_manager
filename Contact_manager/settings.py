1d0ec1d414c30f6c0e5bb986a32859aae01205c6
cmkishores <cmkishores@gmail.com>
2019-11-13 16:16:53 +0530

    Add port 3000 to origin urls

--- Contact_manager/settings.py
+++ Contact_manager/settings.py
@@ -64,8 +64,10 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
-        'https://localhost:8000',
+        'http://localhost:8000',
         'http://127.0.0.1:8000',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
 )
 
 
