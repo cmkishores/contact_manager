f80acf462daf9ce86b788524c764ee0727c9e996
cmkishores <cmkishores@gmail.com>
2019-11-20 08:29:49 +0530

    Remove Search url

--- Contact_manager/urls.py
+++ Contact_manager/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/contacts/$', views.contact_list), #URL for contact listing and creation
     url(r'^api/contacts/(?P<pk>[0-9]+)$', views.contact_detail), #URL for contact update delete or details. 
-    path('api/Search/<str:q>', views.search_contact)  #URL for searching 
 ]
